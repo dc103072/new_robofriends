a4f5c311a8611dd2850ec3dd602587e9bddfab1b
dc103072 <47541752+dc103072@users.noreply.github.com>
2019-02-27 11:06:48 -0600

    load initial files

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-//import * as serviceWorker from './serviceWorker//';
-import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import App from './containers/App';
+//import * as serviceWorker from './serviceWorker';
+//mport registerServiceWorker from './registerServiceWorker';
 
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-registerServiceWorker();
-
+//registerServiceWorker();
+//serviceWorker.unregister();
