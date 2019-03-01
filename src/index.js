7c481edd2dc2914de13368a7e27bc8664c97e39a
dc103072 <47541752+dc103072@users.noreply.github.com>
2019-02-28 18:08:56 -0600

    load initial files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import 'tachyons';
 import App from './containers/App';
 //import * as serviceWorker from './serviceWorker';
 //mport registerServiceWorker from './registerServiceWorker';
+import { searchRobots, requestRobots } from './reducers';
 
+const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = 
+createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+				<Provider store={store}>
+				<App />
+				</Provider>, document.getElementById('root'));
 
 //registerServiceWorker();
 //serviceWorker.unregister();
