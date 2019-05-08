9298ff8995e2cfa52a727c7ebc83aa8d943ade76
dc103072 <47541752+dc103072@users.noreply.github.com>
2019-05-08 14:36:32 -0500

    Load initial files

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './card.css';
+
 
 const Card = ({name, email, id }) =>{
 	return (
-	<div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-        <img alt='robots' src={require(`../Images/${id}.jpg`)} width="250px" height="250px"/>
+
+
+
+
+
+	<div className='bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+       
+	
+    	<div className= 'img'>
+		<img alt='robots' src={require(`../Images/${id}.jpg`)} width="250px" height="250px"/>
+		<p> The Factor for Roger Federer is 25 </p>
+</div>
 		<div>
 		<h2>{name}</h2>
 		<input type="text" name="rank" class="field left" length="1"></input>
